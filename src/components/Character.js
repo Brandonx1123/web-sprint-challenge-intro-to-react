4aab64bdc1282541c1fd22ca6a4faf64aa06ac4e
brandon santos <brandonx1123@gmail.com>
2020-12-12 11:59:12 -0500

    some more

--- src/components/Character.js
+++ src/components/Character.js
@@ -3,9 +3,13 @@
 import React, {useEffect} from 'react'
 import styled, {keyframes} from 'styled-components';
 
-// const innercardDiv = styled.div `
-// width:100%auto;
-// `
+const InnercardDiv = styled.div `
+width: 30%;
+border: 1px solid black;
+margin: 1% auto;
+background:#ffd180;
+box-shadow: 0px 1px 40px -2px mediumpurple;
+`
 
 
 const CardBox = styled.div`
@@ -35,21 +39,32 @@ p{
     font-weight:bold;
 }
 
-.CardDiv {
+/* .CardDiv {
 
 width: 30%;
 border: 1px solid black;
 margin: 1% auto;
-background:wheat;
+background:#ffd180;
 box-shadow: 0px 1px 40px -2px mediumpurple;
-}
+} */
 
 `
 
-// const innercardDiv = styled.div`
-// border: 10px solid white;
-// width:10%;  
-// `
+const Button =styled.button`
+color:white;
+background: #ffb74d;
+font-size: .9rem;
+border-radius: 5px;
+margin:1% auto;
+padding: .5%;
+cursor: pointer;
+transition: ease background 400ms;
+/* outline: 2px dashed green; */
+
+&:hover{
+background:#ff9800;
+}
+`
 
 
 function CharacterCard (props) {
@@ -58,14 +73,14 @@ function CharacterCard (props) {
     return (
       <CardBox>
       {characters.map(characterObj => {
-        return <innercardDiv className ='CardDiv' >
+        return <InnercardDiv className ='CardDiv' >
         <img src={characterObj.image} alt={characterObj.name} /> 
         <h2>{characterObj.name}</h2>
         <p>{characterObj.gender}</p>
         <p>{characterObj.origin.name}</p>
         <p>{characterObj.status}</p>
-        <button onClick={characterObj.results}> Learn More </button>
-      </innercardDiv>
+        <Button onClick={characterObj.results}> Learn More</Button>
+      </InnercardDiv>
         
       })}
       </CardBox>
